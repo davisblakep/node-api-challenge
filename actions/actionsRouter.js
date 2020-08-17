507783724dbdb688210aeaf8fb86f88ea1079b56
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-08-16 20:07:59 -0400

    Actions Router is set up with CRUD and is operational. All CRUD has been implemented.  Needs Middleware items

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -1,4 +1,71 @@
 const express = require("express");
 const router = express.Router();
+const actions = require("../data/helpers/actionModel");
+
+router.get("/api/actions", (req, res) => {
+  actions
+    .get()
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get("/api/actions/:id", (req, res) => {
+  actions
+    .get(req.params.id)
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post("/api/projects/:id/actions", (req, res, next) => {
+  actions
+    .insert({
+      project_id: req.params.id,
+      description: req.body.description,
+      notes: req.body.notes,
+      completed: false,
+    })
+    .then((post) => {
+      res.status(201).json(post);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.put("/api/actions/:id", (req, res, next) => {
+  actions
+    .update(req.params.id, req.body)
+    .then((item) => {
+      res.status(200).json(item);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.delete("/api/actions/:id", (req, res, next) => {
+  actions
+    .remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({
+          message: "The action has been deleted.",
+        });
+      } else {
+        res.status(404).json({
+          message: "The action could not be found",
+        });
+      }
+    })
+    .catch(next);
+});
 
 module.exports = router;
