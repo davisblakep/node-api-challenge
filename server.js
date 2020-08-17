1616d80b68135b3a597243dab79073f1064f494f
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-08-16 21:47:09 -0400

    Added .env file and working towards deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const actionsRouter = require("./actions/actionsRouter");
 const projectsRouter = require("./projects/projectsRouter");
 
